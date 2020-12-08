59500c7571ebfaf729c800350f8d31089dc9705c
Mikhail Gorbunov <GorbunovMU@ukr.net>
2020-12-08 12:35:37 +0200

    Ex13

--- src/main/java/com/luxoft/jva/multithreading/ch06_atomic/Exercise13.java
+++ src/main/java/com/luxoft/jva/multithreading/ch06_atomic/Exercise13.java
@@ -28,9 +28,77 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class Exercise13 {
 
-	public static void main(String[] args) {
+	public static int KOL_ITERATIONS = 100;
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("Game starting...");
 		// your code goes here
+		BallAtomic ballAtomic = new BallAtomic(0, 1);
+		Thread pingThread = new Thread(new PingAtomic(ballAtomic));
+		Thread pongThread = new Thread(new PongAtomic(ballAtomic));
+
+		pingThread.start();
+		pongThread.start();
+
+		pingThread.join();
+		pongThread.join();
+
+		System.out.println(ballAtomic);
 	}
 
 }
 
+class BallAtomic {
+	AtomicInteger ping;
+	AtomicInteger pong;
+
+	public BallAtomic(int ping, int pong) {
+		this.ping = new AtomicInteger();
+		this.pong = new AtomicInteger();
+		this.ping.set(ping);
+		this.pong.set(pong);
+	}
+
+	@Override
+	public String toString() {
+		return "Ball{" +
+				"ping=" + ping +
+				", pong=" + pong +
+				'}';
+	}
+}
+
+class PongAtomic implements Runnable {
+	private BallAtomic ball;
+
+	PongAtomic(BallAtomic ball) {
+		this.ball = ball;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 1; i < Exercise13.KOL_ITERATIONS + 1; i++) {
+			while (ball.ping.get() != i) {}
+			ball.pong.addAndGet(1);
+		}
+
+	}
+}
+
+
+class PingAtomic implements Runnable {
+	private BallAtomic ball;
+
+	public PingAtomic(BallAtomic ball) {
+		this.ball = ball;
+	}
+
+	@Override
+	public void run() {
+		for (int i = 1; i < Exercise13.KOL_ITERATIONS + 1; i++) {
+			while (ball.pong.get() != i) {
+			}
+			ball.ping.addAndGet(1);
+		}
+	}
+}
